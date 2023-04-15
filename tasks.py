363100ed912f5a2599a3408a9eca1aba99bb0f7d
Otso <24369914+0tso@users.noreply.github.com>
2023-04-15 19:02:17 +0300

    Basic unit testing added, tasks expanded

--- tasks.py
+++ tasks.py
@@ -3,3 +3,15 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python -m tilemap_editor.app", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest tilemap_editor/", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest tilemap_editor/")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
