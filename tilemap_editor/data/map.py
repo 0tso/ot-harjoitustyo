cda26accaaac7c16bf6de9e8d80066e9b9a09386
Otso <24369914+0tso@users.noreply.github.com>
2023-04-17 19:06:48 +0300

    A hardcoded test tilemap added

--- tilemap_editor/data/map.py
+++ tilemap_editor/data/map.py
@@ -1,6 +1,13 @@
+
+# TESTING
+import os
+TEST_GRASS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "assets", "grass.png")
+TEST_MAP = {(0,0): TEST_GRASS, (1, 1): TEST_GRASS, (1, 2): TEST_GRASS, (5,5): TEST_GRASS, (0, 1): TEST_GRASS}
+
 class Map:
     def __init__(self, tiles):
-        self.tiles = tiles
+        # TESTING
+        self.tiles = TEST_MAP
     
     def get_tiles(self, x, y, width, height):
         return [self.get_tile(x+X, y+Y) for Y in range(height) for X in range(width)]
